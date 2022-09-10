5dfcb9736bff1e92423432cb433848c3c9140cdc
IbraHam733 <98653954+ibrahim2375@users.noreply.github.com>
2022-09-10 11:45:08 +0200

    some styles

--- src/components/Dashboard/Card.js
+++ src/components/Dashboard/Card.js
@@ -1,22 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 //mui icons 
 import CreditCardIcon from '@mui/icons-material/CreditCard';
 import Typography from '@mui/material/Typography'
+
 //css
 import '../../css/Card.css'
-
-function Card() {
+let totalPaidCount = 0;
+function Card({ orders }) {
+    let [totalPaid, setTotalPaid] = useState(0)
+    useEffect(() => {
+        let finish = false;
+        if (!finish) {
+            const total = orders?.map((order) => {
+                return setTotalPaid(totalPaidCount += (order?.quantity * order?.price))
+            })
+            if (total?.length === orders?.length) {
+                finish = true;
+                totalPaidCount = 0;
+            }
+        }
+    }, [orders])
     return (
         <div className='card'>
             <div className="card_head">
                 <p className='muted'>Available Balance</p>
             </div>
             <div className="card_middle">
-                <Typography variant='h4' component='h3'> $55.70 </Typography>
+                <Typography variant='h4' component='h3'> ${totalPaid} </Typography>
             </div>
             <div className="card_footer">
                 <p>************42424</p>
-                <CreditCardIcon  />
+                <CreditCardIcon />
             </div>
 
         </div>
